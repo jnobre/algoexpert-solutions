6e0b214436df95650ad516f2a0f9818ebbaab973
João Nobre <joaoanobre@gmail.com>
2022-09-14 18:53:44 +0200

    Add solutions for Array of Products, First Duplicate and Longest Peak problems

---
+++ medium/FirstDuplicate.go
@@ -0,0 +1,19 @@
+package main
+
+func FirstDuplicateValue(array []int) int {
+	for _, value := range array {
+		absValue := abs(value)
+		if array[absValue-1] < 0 {
+			return absValue
+		}
+		array[absValue-1] *= -1
+	}
+	return -1
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
